8215725d67f4c7f5ab2894960a228fbd69dcb0d6
yijuncai987 <yijuncai1987@gmail.com>
2025-11-08 00:52:29 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,9 +18,3 @@ export default function RootHome() {
   return (EnHome as any)({ params: Promise.resolve({ lang: 'en' }) })
 }
 
-import { redirect } from 'next/navigation'
-import { i18n } from '@/i18n/config'
-
-export default function RootPage() {
-  redirect(`/${i18n.defaultLocale}`)
-}
